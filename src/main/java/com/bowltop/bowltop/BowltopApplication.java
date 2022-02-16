4189e9e83f31e57ab816b85c56a6533a0d578c8c
park <s10e01s@naver.com>
2022-02-17 00:20:24 +0900

    스프링과 JPA기반 웹 애플리케이션 개발환경 구축

---
+++ src/main/java/com/bowltop/bowltop/BowltopApplication.java
@@ -0,0 +1,13 @@
+package com.bowltop.bowltop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowltopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowltopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bowltop/bowltop/BowltopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowltop.bowltop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowltopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
