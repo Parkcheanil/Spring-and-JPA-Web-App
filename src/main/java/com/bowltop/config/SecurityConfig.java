e9dcb97a25599cf3061513703d987900a4ab86d1
park <s10e01s@naver.com>
2022-02-19 01:28:13 +0900

    회원가입 뷰

--- src/main/java/com/bowltop/config/SecurityConfig.java
+++ src/main/java/com/bowltop/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.bowltop.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
+    //httpSecurity 패턴은 보안처리
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token", "/email-login", 
@@ -19,4 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .anyRequest().authenticated();
     }
     
+    @Override
+    //WebSecurity 패턴은 보안예외처리(정적리소스, HTML)
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 }
