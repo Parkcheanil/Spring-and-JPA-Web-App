23213b6eec510aa51ae7406d3cbf7daf0ec8546c
park <s10e01s@naver.com>
2022-02-22 01:19:50 +0900

    회원가입 서브밋 검증 - 1

--- src/main/java/com/bowltop/account/AccountController.java
+++ src/main/java/com/bowltop/account/AccountController.java
@@ -1,8 +1,13 @@
 package com.bowltop.account;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
@@ -13,4 +18,14 @@ public class AccountController {
         return "account/sign-up";
     }
     
+    @PostMapping("/sign-up")
+    @Validated
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        // TODO회원 가입 처리
+        return "redirect:/";
+    }
 }
