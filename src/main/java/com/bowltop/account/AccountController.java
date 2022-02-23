d4e48e1b82e46c4d3b6901fb05e5ba2051538baa
park <s10e01s@naver.com>
2022-02-24 00:03:19 +0900

    회원가입 리팩토링 및 테스트

--- src/main/java/com/bowltop/account/AccountController.java
+++ src/main/java/com/bowltop/account/AccountController.java
@@ -1,11 +1,6 @@
 package com.bowltop.account;
 
 import javax.validation.Valid;
-
-import com.bowltop.domain.Account;
-
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -14,7 +9,6 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -22,8 +16,8 @@ import lombok.RequiredArgsConstructor;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
+    
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -41,26 +35,10 @@ public class AccountController {
     public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
         if (errors.hasErrors()) {
             return "account/sign-up";
-        }
-        
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODOencoding 해야함
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();
-        Account newAccount = accountRepository.save(account);
-        
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("볼탑, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
-        
+        }        
+        accountService.processNewAccount(signUpForm);
 
         return "redirect:/";
     }
+
 }
