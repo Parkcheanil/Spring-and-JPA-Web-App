5bb0f606bdb3a7a347acb21b29f2c07a3575a832
Park <s10e01s@naver.com>
2022-03-01 16:53:00 +0900

    회원가입 패스워드 인코딩

--- src/test/java/com/bowltop/account/AccountControllerTest.java
+++ src/test/java/com/bowltop/account/AccountControllerTest.java
@@ -2,7 +2,7 @@ package com.bowltop.account;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito.Then;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,12 +11,16 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.bowltop.domain.Account;
 
 
 @SpringBootTest
@@ -44,7 +48,7 @@ class AccountControllerTest {
 
     @DisplayName("회원 가입 처리 - 입력값 정상")
     @Test
-    void signUpSubmit_with_wrong_input() throws Exception {
+    void signUpSubmit_with_correct_input() throws Exception {
         mockMvc.perform(post("/sign-up")
             .param("nickname", "chenil")
             .param("email", "chenil@email.com")
@@ -53,8 +57,10 @@ class AccountControllerTest {
             .andExpect(status().is3xxRedirection()) 
             .andExpect(view().name("redirect:/"));
 
-        assertTrue(accountRepository.existsByEmail("chenil@email.com"));
-        // Then(javaMailSender).should().send.(any(SimpleMailMessage.class));
+        Account account = accountRepository.findByEmail("chenil@email.com");
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), "12345678");
+        BDDMockito.then(javaMailSender).should().send(any(SimpleMailMessage.class));
 
     }
 
